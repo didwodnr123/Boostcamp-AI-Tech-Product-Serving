6aaddce8cad6ff328fa2111697ccdfe2e32a2e46
Jadon <didwodnr123@gmail.com>
2021-12-18 04:30:00 +0900

    UPDATE: skip if label is none

--- practice/03-streamlit-fastapi/app/main.py
+++ practice/03-streamlit-fastapi/app/main.py
@@ -16,15 +16,22 @@ def hello_world():
 def inference(keyword):
     results = []
     comments = retrieve_comments(keyword)
+    
     for comment in comments[:10]:
         output = pipe(comment)
-        results.append(
-            {
-                'comment': comment,
-                'label': output[0]['label'],
-                'score': output[0]['score']
-            }
-        )
+    
+        if output[0]['label'] == 'none':
+            continue
+    
+        else:
+            results.append(
+                {
+                    'comment': comment,
+                    'label': output[0]['label'],
+                    'score': output[0]['score']
+                }
+            )
+    
     return results
 
 
