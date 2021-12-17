36d5a7d0166625a7f6b6a3a7a1c3d3d0cd0bcdd4
Jadon <didwodnr123@gmail.com>
2021-12-17 13:24:56 +0900

    UPDATE: rename directory

--- practice/01-streamlit/predict.py
+++ practice/01-streamlit/predict.py
@@ -9,7 +9,7 @@ def get_pipeline():
     device = 0 if torch.cuda.is_available() else -1
 
     model_path = '/Users/yangjaeug/Desktop/GitHub/Product-Serving/assets/comments_task/model.pt'
-    config_path = '/practice/streamlit/config.json'
+    config_path = '/practice/01-streamlit/config.json'
 
     model = torch.load(model_path, map_location=torch.device('cpu'))
     model_config = read_json(config_path)
--- practice/03-streamlit-fastapi/app/predict.py
+++ practice/03-streamlit-fastapi/app/predict.py
@@ -1,6 +1,6 @@
 from transformers import pipeline, AutoTokenizer
 import torch
-# import streamlit as st
+# import 01-streamlit as st
 from utils import read_json
 
 
@@ -9,7 +9,7 @@ def get_pipeline():
     device: int = 0 if torch.cuda.is_available() else -1
 
     model_path: str = '/Users/yangjaeug/Desktop/GitHub/Product-Serving/assets/comments_task/model.pt'
-    config_path: str = '/practice/streamlit/config.json'
+    config_path: str = '/practice/01-streamlit/config.json'
 
     model = torch.load(model_path, map_location=torch.device('cpu'))
     model_config = read_json(config_path)
