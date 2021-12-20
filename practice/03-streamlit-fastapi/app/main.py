3870fae59c45633c11cd3e55c9fef7973e2d9c7e
jadon <didwodnr123@gmail.com>
2021-12-20 14:20:59 +0000

    test

--- practice/03-streamlit-fastapi/app/main.py
+++ practice/03-streamlit-fastapi/app/main.py
@@ -47,4 +47,4 @@ def get_sample(keyword):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=6006, reload=True)  
+    uvicorn.run('main:app', host='0.0.0.0', port=2227, reload=True)  
